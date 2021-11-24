96ab9bfb9d9896dc5777fba39e47b0646a580fe5
Brun096 <Brunoh96@outlook.it>
2021-11-24 16:58:23 +0100

    ho creato una classe per calcolare gli scrutini

---
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -0,0 +1,28 @@
+package it.corso.valutazioni;
+
+class CalcolaValutazione {
+
+	int idStudente;
+	int assenze;
+	double mediaVoti;
+	String studente;
+	
+	//calcolare le assenze e i voti
+	
+	CalcolaValutazione(int idStudente, int assenze, double mediaVoti ){
+		
+		this.idStudente=idStudente;
+		this.assenze=assenze;
+		this.mediaVoti=mediaVoti;
+		
+		
+		if ((assenze>25 && assenze<50) && mediaVoti>2) {
+			studente="Promosso";
+		}else if(assenze<25 && mediaVoti>=2){
+			studente="Promosso";
+		}else{
+			studente="Bocciato";
+		}
+	 System.out.println(studente);
+	}
+}
